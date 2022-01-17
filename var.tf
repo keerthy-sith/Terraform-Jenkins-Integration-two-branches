7bea22fdeba3e90c1f9e2bc7f4c10a78c62c7b70
EC2 Default User <ec2-user@ip-172-31-83-230.ec2.internal>
2022-01-17 17:12:42 +0000

    main

---
+++ var.tf
@@ -0,0 +1,56 @@
+variable "region" {
+  type    =string
+  default = "us-east-1"
+}
+variable "ami_id" {
+  type = map
+  default = {
+    us-east-1    = "ami-08e4e35cccc6189f4"
+    eu-west-2    = "ami-02d03ce209db75523"
+    eu-central-1 = "ami-05cafdf7c9f772ad2"
+  }
+}
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+variable "key_name" {
+  type    = string
+  default = "TF-1"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  type        = string
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_classiclink" {
+  description = "Should be true to enable ClassicLink for the VPC. Only valid in regions and accounts that support EC2 Classic."
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = string
+  default     = "Vpc-custom-demo"
+}
+
